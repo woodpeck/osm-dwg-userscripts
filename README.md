3fa129d61b907f58b2089ba4776dc73b07a359a8
Frederik Ramm <frederik@remote.org>
2019-06-09 21:43:42 +0200

    update docs

--- README.md
+++ README.md
@@ -1,10 +1,28 @@
 # osm-dwg-userscripts
-GreaseMonkey User Scripts for DWG (OSM moderator) usage
+User Scripts for DWG (OSM moderator) usage
 
-### dwg_issue2ticket.user.js
+The scripts have been developed for "Violentmonkey" on Firefox. "Greasemonkey" does not work with all scripts.
 
-Intended for use on OSM website "issues" page. Adds a "create OTRS ticket" link near the top of the page. Creates OTRS tickets using a configured OTRS web service.
+## dwg_issue2ticket.user.js
+
+Intended for use on the OSM website "issues" page. Adds a "create OTRS ticket" link near the top of the page. Creates OTRS tickets using a configured OTRS web service.
 
 ![Screenshot](dwg_issue2ticket.png)
 
 The script stores OTRS user name and password in Greasemonkey's internal variable storage. 
+
+To install:
+
+Activate the "Violentmonkey" extension, then navigate to https://github.com/woodpeck/osm-dwg-userscripts/raw/master/dwg_issue2ticket.user.js
+
+## dwg_issue2ticket.user.js
+
+Intended for use on OTRS. When adding a "note" to a ticket, allows to create an OSM user-to-user message from the note text. The recipient user name is taken from the note subject line, which must end in "to <username>". The message body is taken from the note body, except if the first line of the note body is "Subject: XXX", in which case this is used as a subject line for the message.
+
+![Screenshot](dwg_usermessage.png)
+
+The script requires the disabling of frame-related security features in the browser (because it relies on controlling an instance of the OSM web site loaded into an invisible IFrame, and the OSM web site uses headers to forbid that).
+
+To install: 
+
+Activate the "Violentmonkey" extension, then navigate to https://github.com/woodpeck/osm-dwg-userscripts/raw/master/dwg_usermessage.user.js
--- dwg_usermessage.user.js
+++ dwg_usermessage.user.js
@@ -5,15 +5,34 @@
 // @match    https://otrs.openstreetmap.org/otrs/index.pl?Action=AgentTicketNote;TicketID=*
 // @match    https://www.openstreetmap.org/message/new/*
 // @match    https://www.openstreetmap.org/messages/inbox
+// @match    https://www.openstreetmap.org/login?referer=%2Fmessages%2Finbox
 // @require  https://unpkg.com/turndown/dist/turndown.js
 // ==/UserScript==
 
+// written by Frederik Ramm <frederik@remote.org>, Public Domain
+// 
 // This script is run twice - once in OTRS, where it creates an iframe
-// calling the OSM web site, in which is then run again.
-
+// calling the OSM web site, and a second time in that OSM web site. 
+// This could equally well be engineered as two different scripts but it
+// is easier to maintain as one.
+//
+// In order to control the OSM web site in an iframe, we need to override
+// both the "content security policy" and the "x-frame-options" headers,
+// which the OSM web site uses to tell the browser that it does not want 
+// to be run in a frame. There are different ways to achieve this. For 
+// Firefox, one option is:
+// 1. go to "about:config" and set "security.csp.enable" to "false" -
+//    note that this disables CSP for all web sites
+// 2. install the "Ignore X-Frame-Options Header" add-on and make set
+//    the whitelist to "https://www.openstreetmap.org/*"
+// There migtht be other addons that let you filter out arbitrary headers
+// for specific websites only; another option is using a local proxy that
+// removes these headers.
+// ----------------------------------------------------------------------
+                                                 
 let iframe;
 let statusmsg;
-let copybutton;
+let messagebutton;
 
 if (window.location.href.startsWith("https://otrs.openstreetmap.org")) {
   otrs_aspect();
@@ -21,40 +40,72 @@ if (window.location.href.startsWith("https://otrs.openstreetmap.org")) {
   osm_aspect();
 }
 
-// this is the part that runs in OTRS
+// This is the part that runs in OTRS.
+// Its main job is to send messages to the OSM iframe when
+// (a) the "subject" line is filled out, potentially containing a recipient name
+// (b) the "send message" button is clicked.
 
 function otrs_aspect()
 {
+  // parse OTRS page
   let form = document.getElementById("Compose");
   if (!form) {
     console.log("OTRS page does not look right");
     throw("");
   }
+  
+  // create OSM iframe, point it to the "inbox" page where it will receive
+  // a redirect to login if not logged in.
   iframe = document.createElement("iframe");
-  iframe.setAttribute("src", "https://www.openstreetmap.org/message/new/woodpeck");
-  iframe.style.width = "100px";
-  iframe.style.height = "100px";
-  //iframe.addEventListener("load", iframe_loaded);
+  iframe.setAttribute("src", "https://www.openstreetmap.org/messages/inbox");
+  iframe.style.width = "100%";
+  iframe.style.height = "400px";
+  iframe.style.display = "none";
+
+  // add frame to page
   form.parentNode.appendChild(iframe);
+
+  // set us up for receiving messages from the iframe
+  window.addEventListener("message", receiveMessageFromIframe, false);
+  
+  // create a new button and add it to the existing button bar
   let submit = document.getElementById("submitRichText");
-  copybutton = document.createElement("button");
-  copybutton.innerHTML = "<span>create OSM message</span>";
-  copybutton.disabled = true;
-  submit.parentNode.insertBefore(copybutton, submit);
-  let span = document.createElement("span");
-  span.innerHTML = "&nbsp;/&nbsp;"
-  submit.parentNode.insertBefore(span, submit);
-  copybutton.addEventListener("click", copy_text);
-  copybutton.classList.add("CallForAction")
+  messagebutton = document.createElement("button");
+  messagebutton.innerHTML = "<span>create OSM message</span>";
+  messagebutton.disabled = true;
+  messagebutton.addEventListener("click", send_osm_message);
+  messagebutton.classList.add("CallForAction")
+  submit.parentNode.insertBefore(messagebutton, submit);
+  submit.parentNode.insertBefore(document.createTextNode(" / "), submit);
+
+  // identify the "subject" line and add an event listener
   let subject = document.getElementById("Subject");
   subject.addEventListener("blur", eval_subject);
-  statusmsg = document.createElement("div");
-  statusmsg.innerHTML="OSM status not available";
-  subject.parentNode.appendChild(statusmsg);
   
-  window.addEventListener("message", receiveMessageFromIframe, false);
+  // add a "status bar" below the subject line, for feedback from the 
+  // OSM iframe
+  statusbar = document.createElement("div");
+  statusmsg = document.createElement("span");
+  statusmsg.innerHTML="OSM status not available - perhaps a CSP or X-Frame-Options issue?";
+  iframebox = document.createElement("input");
+  iframebox.type = "checkbox";
+  iframeboxlabel = document.createElement("label");
+  iframeboxlabel.appendChild(document.createTextNode("show OSM iframe"));
+  iframeboxlabel.appendChild(iframebox);
+  iframebox.checked = false;
+  iframebox.addEventListener("change", function(e) { 
+    iframe.style.display = e.target.checked ? "inline" : "none";  
+  });
+  statusbar.appendChild(statusmsg);
+  statusbar.appendChild(document.createTextNode(" | "));
+  statusbar.appendChild(iframeboxlabel);
+  subject.parentNode.appendChild(statusbar);
 }
 
+// if the subject line is filled out with something matching "to ...",
+// assume what comes after "to" is an user name and trigger iframe to 
+// open a "send message to ..." user. Note this will generate an error
+// in case the user does not exist, which will be reported back properly.
 function eval_subject(e)
 {
   let text = e.target.value;
@@ -64,15 +115,19 @@ function eval_subject(e)
   }
 }
 
-function copy_text(e)
+// this is called when the "send OSM message" button is clicked. It utilizes
+// a 3rd party library called "turndown" to convert OSRM's rich text HTML to
+// a "markdown" message for OSM. If the message body's first line is something
+// like "Subject: XXX", then XXX becomes the subject for the OSM message.
+function send_osm_message(e)
 {
   e.preventDefault();
   let subject = document.getElementById("Subject");
   let rt = document.getElementById("RichText");
   let win = iframe.contentWindow;
-  let turndownService = new TurndownService()
-  let markdown = turndownService.turndown(rt.value)
-  let message = { "body" : markdown, "subject" : "" };
+  let turndownService = new TurndownService();
+  let markdown = turndownService.turndown(rt.value);
+  let message = { "body" : markdown, "subject" : "Message from DWG" };
   let matches = markdown.match(/^\s*subject:\s*([^\n\r]*?)(\r?\n)+([\s\S]*)/i);
   if (matches) {
     message = { "body" : matches[3], "subject" : matches[1] }
@@ -80,12 +135,16 @@ function copy_text(e)
   win.postMessage(JSON.stringify(message), "*");
 }
 
+// This is called when the iframe sends a message. It updates the status message,
+// and enables the "send message" button only if the iframe reports ready.
 function receiveMessageFromIframe(event)
 {
   let json=JSON.parse(event.data);
   statusmsg.innerHTML = "OSM says: " + json.text;
-  copybutton.disabled = !json.ok;
+  messagebutton.disabled = !json.ok;
 }
+
+// ------------------------------------------------------------------------------
 // this is the part that runs on the OSM website
 
 function osm_aspect()
@@ -95,26 +154,43 @@ function osm_aspect()
     return;
   }
 
+  // this code tries to detect the various states the OSM web site can be in:
+  // * we are not logged in
+  // * we are logged in and have not yet requested composing a message
+  // * we have requested composing a message to an invalid user
+  // * we have requested composing a message to a valid user
+  // * we have successfully sent the message
+  // It generates appropriate messages back to the container (OTRS) where
+  // the status is displayed in the status bar under the subject line. Only
+  // if we are in the "have requested composing messag to valid user" status
+  // do we send "ok=true" which enables the "send message" button on OTRS.
+  
+  // pessimistic default
   let message = { "ok" : false, "text" : "The OSM page cannot be parsed properly." };
-  let flash = document.evaluate("//div[@class='flash notice']//div[@class='message']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  if (flash) {
-    message = { "ok" : false, "text" : flash.innerHTML };
+  if (window.location.href == "https://www.openstreetmap.org/login?referer=%2Fmessages%2Finbox") {
+      message = { "ok" : false, "text" : "Please log in to OSM in another tab, then reload this page" };
+  } else if (window.location.href == "https://www.openstreetmap.org/messages/inbox") {
+    let flash = document.evaluate("//div[@class='flash notice']//div[@class='message']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    let userbutton = document.evaluate("//span[@class='user-button']//span[@class='username']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (flash) {
+      message = { "ok" : false, "text" : flash.innerHTML };
+    } else if (userbutton) {
+      message = { "ok" : false, "text" : "ready to send, logged in as " + userbutton.innerHTML }
+    }
   } else {
-     let h = document.evaluate("//div[@id='content']//*[(self::h1 or self::h2)]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-     if (h) {
-       message = { "ok" : (h.nodeName == "H2"), "text" : h.innerHTML };
-     }
+    let h = document.evaluate("//div[@id='content']//*[(self::h1 or self::h2)]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    message = { "ok" : (h.nodeName == "H2"), "text" : h.innerText };
   }
   parent.postMessage(JSON.stringify(message), "*");
   if (message.ok) window.addEventListener("message", receiveMessageFromContainer, false);
 }
 
+// the only kind of message we receive is a "send OSM message" command;
+// parse and send the message.
 function receiveMessageFromContainer(event) 
 {
     let json = JSON.parse(event.data);
     document.getElementById("message_title").value = json.subject;
     document.getElementById("message_body").value = json.body;
     document.getElementById("new_message").submit();
-    
 }
-
